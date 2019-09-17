df655c73ed962c68bece098e6eb4a3d964591bdb
CodeSchool2019 <51709180+CodeSchool2019@users.noreply.github.com>
2019-09-17 18:40:37 +0530

    Add files via upload

---
+++ resources/js/scripts.js
@@ -0,0 +1,13 @@
+/* global $ */
+/* eslint-env jquery*/
+$('document').ready(function() {
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+});
